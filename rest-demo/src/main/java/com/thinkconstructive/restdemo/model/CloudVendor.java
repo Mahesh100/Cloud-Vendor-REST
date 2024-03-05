9b78d94b4ed509df876ec6b3d2211d806ff29922
Mahesh Kakde <maheshkakde100@gmail.com>
2024-03-05 18:57:13 +0530

    files added

---
+++ rest-demo/src/main/java/com/thinkconstructive/restdemo/model/CloudVendor.java
@@ -0,0 +1,12 @@
+package com.thinkconstructive.restdemo.model;
+
+public class CloudVendor {
+
+    private String VendorId;
+    private String VendorName;
+
+    private String VendorAddress;
+
+    private  String VendorPhoneNumber;
+
+}
